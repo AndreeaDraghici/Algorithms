37dc67bcb92fba507ea6d21cc55054445cfdc34d
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-04 20:52:10 +0300

    Create HexagonalNumbers.java

---
+++ HexagonalNumbers/src/hexagonalnumbers/HexagonalNumbers.java
@@ -0,0 +1,14 @@
+package hexagonalnumbers;
+
+public class HexagonalNumbers {
+
+	public static void main(String[] args) {
+		for(int n=1;n<=10;n++)
+		{
+			int number=n*(2*n-1);
+			System.out.println(number+"");
+		}
+        System.out.println("");
+	}
+
+}

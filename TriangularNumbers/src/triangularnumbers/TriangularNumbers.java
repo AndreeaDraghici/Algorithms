efe7aa7f2db2296164f6a7d47b2a1c92bfa29c62
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-04 20:52:44 +0300

    Create TriangularNumbers.java

---
+++ TriangularNumbers/src/triangularnumbers/TriangularNumbers.java
@@ -0,0 +1,14 @@
+package triangularnumbers;
+
+public class TriangularNumbers {
+
+	public static void main(String[] args) {
+		for(int n=1;n<=10;n++)
+		{
+			int number=n*(n+1)/2;
+			System.out.println(number+"");
+		}
+		System.out.println(" ");
+	}
+
+}
